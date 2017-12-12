2f1ac813ca25f6a0c90961df51da5c7636d456f5
Unknown <cocoscorpio94@hotmail.fr>
2017-12-12 10:41:33 +0100

    12/12/2017 10:41
    Query creating.

--- src/AppBundle/Repository/CourseRepository.php
+++ src/AppBundle/Repository/CourseRepository.php
@@ -8,10 +8,18 @@ class CourseRepository extends \Doctrine\ORM\EntityRepository
     public function testQuery()
     {
         /*
-         *
+         *create query builder : constructeur de requête
+         *      Définir l'Alias de l'entité
+         * getQuery : éxécution de la requête / appel en avant dernière position
+         *Récupération des résultats :
+         *      getResult : renvoie un array d'objets
          *
          **/
-        $result = $this->createQueryBuilder();
+        $query = $this->createQueryBuilder('courseAlias')
+            ->getQuery()
+            ->getResult()
+        ;
 
+        //retour des résultate
     }
 }
